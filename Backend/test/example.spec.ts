fa087cfbfb42c9e56545dacc417517123d1c36cc
Sardor Allamurotov <sardorallamurotov0@gmail.com>
2026-09-22 20:35:32 +0500

    test

--- Backend/test/example.spec.ts
+++ Backend/test/example.spec.ts
@@ -3,9 +3,4 @@ describe('Backend Sample Unit Test', () => {
     const sum = (a: number, b: number) => a + b;
     expect(sum(2, 3)).toBe(5);
   });
-
-  it('should verify sample backend logic', () => {
-    const isServiceRunning = true;
-    expect(isServiceRunning).toBeTruthy();
-  });
 });
