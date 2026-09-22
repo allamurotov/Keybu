5c351d946a3219521718011f92401fdefe34bf5b
Sardor Allamurotov <sardorallamurotov0@gmail.com>
2026-09-22 20:39:25 +0500

    test

--- Backend/test/example.spec.ts
+++
@@ -1,6 +0,0 @@
-describe('Backend Sample Unit Test', () => {
-  it('should verify basic math operation', () => {
-    const sum = (a: number, b: number) => a + b;
-    expect(sum(2, 3)).toBe(5);
-  });
-});
